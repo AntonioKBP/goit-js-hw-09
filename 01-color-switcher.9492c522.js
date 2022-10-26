c5158a664a6baefdaf01224b7656a1f905d2be48
AntonioKBP <105855258+AntonioKBP@users.noreply.github.com>
2022-10-26 17:35:46 +0000

    Deploying to gh-pages from @ AntonioKBP/goit-js-hw-09@418edb808d787e3645dd2e4f5efd22f578dfad5f 🚀

--- 01-color-switcher.9492c522.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",n),e.addEventListener("click",(function(){t.removeAttribute("disabled",""),clearInterval(o)}));const o=setInterval(n,1e3);function n(){t.setAttribute("disabled",""),document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
-//# sourceMappingURL=01-color-switcher.9492c522.js.map
---
+++ 01-color-switcher.cb9d5361.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled",""),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled",""),e.setAttribute("disabled",""),clearInterval(d)}));let d;e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.cb9d5361.js.map
