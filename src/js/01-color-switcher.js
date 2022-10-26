4ac31de720d0cc042469e730fcbdbceb1923caa5
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-26 19:47:45 +0300

    upd one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', startBackGroundChange);
+stopBtn.addEventListener('click', stopBackGroundChange);
+
+// const myInterval = setInterval(() => {}, 1000);
+
+const myInterval = setInterval(startBackGroundChange, 1000);
+
+function startBackGroundChange() {
+  startBtn.setAttribute('disabled', '');
+  document.body.style.backgroundColor = getRandomHexColor();
+  //   setInterval(myInterval);
+}
+
+function stopBackGroundChange() {
+  startBtn.removeAttribute('disabled', '');
+  clearInterval(myInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
