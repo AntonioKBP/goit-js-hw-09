78fcab7056996a2a5e0f89c0b610dedb0f290349
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-29 19:00:19 +0300

    started 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,3 +12,24 @@ const options = {
 };
 
 flatpickr('#datetime-picker', options);
+
+const startBtn = document.querySelector('button[data-start]');
+const inputRef = document.querySelector('#datetime-picker');
+const daysOutput = document.querySelector('span[data-days]');
+const hoursOutput = document.querySelector('span[data-hours]');
+const minsOutput = document.querySelector('span[data-minutes]');
+const secsOutput = document.querySelector('span[data-seconds]');
+
+let timerId;
+// startBtn.setAttribute('disabled', '');
+startBtn.addEventListener('click', startTimer);
+
+function startTimer() {
+  const startTime = Date.now();
+
+  const timerId = setInterval(() => {
+    console.log('check');
+  }, 1000);
+}
+
+const timer = {};
