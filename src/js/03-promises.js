bdd3cc9f8edccdeb5a0bdb876be9eb6e08012eac
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-06 19:08:29 +0200

    upd before notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,13 +20,18 @@ formRef.addEventListener('submit', onSubmitClick);
 function onSubmitClick(evt) {
   evt.preventDefault();
   formElements = evt.currentTarget.elements;
-  delayValue = formElements.delay.value;
-  console.log(delayValue);
-  createPromise(2, 1500)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+  delayValue = Number(formElements.delay.value) * 1000;
+  stepValue = Number(formElements.step.value) * 1000;
+  amountValue = Number(formElements.amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayValue += stepValue;
+  }
 }
